5a58f8924af41e0e6520aeb6d83485b4bd79e62b
cchu70 <cchu@broadinstitute.org>
2024-06-20 00:13:20 -0400

    Update setup.py fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SimutatedTumorData",
+    name="SimulatedTumorData",
     version="0.0.1",
     author="Kristy Schlueter-Kuck and Claudia Chu",
     author_email="cchu@broadinstitute.org",
